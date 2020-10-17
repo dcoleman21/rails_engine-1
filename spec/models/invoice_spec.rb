b836bb28f8897c03ed2cdb7fab77cb9bdc549e59
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-17 16:43:49 -0600

    Add Invoice spec
    - validates status
    - relationship Customer/Merchant: belongs to

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe Invoice, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :status}
+  end
+
+  describe "relationships" do
+    #it { should have_many :databse}
+    it { should belong_to :customer}
+    it { should belong_to :merchant}
+  end
+end
