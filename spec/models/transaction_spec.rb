c6deedcc0b68d26c776816a7b822f50ae24e803e
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-17 17:19:21 -0600

    Update all files for rubocop autocorrecct

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  describe "validations" do
-    it { should validate_presence_of :card}
-    it { should validate_presence_of :card_exp}
-    it { should validate_presence_of :result}
+  describe 'validations' do
+    it { should validate_presence_of :card }
+    it { should validate_presence_of :card_exp }
+    it { should validate_presence_of :result }
   end
 
-  describe "relationships" do
-    it { should belong_to :invoice}
+  describe 'relationships' do
+    it { should belong_to :invoice }
   end
 end
