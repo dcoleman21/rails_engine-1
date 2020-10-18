15a670f48523892e0b045a771828dfb97d93367c
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 17:58:33 -0600

    Update item request spec for show endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -9,7 +9,7 @@ describe "Items API" do
     expect(response).to be_successful
 
     items = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(items[:data].count).to eq(5)
 
     items[:data].each do |item|
@@ -37,5 +37,30 @@ describe "Items API" do
       expect(item[:attributes]).to have_key(:updated_at)
       expect(item[:attributes][:updated_at]).to be_a(String)
     end
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to eq("item")
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:created_at)
+    expect(item[:attributes][:created_at]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:updated_at)
+    expect(item[:attributes][:updated_at]).to be_a(String)
   end
 end
