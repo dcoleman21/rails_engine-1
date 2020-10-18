0df7a27309bd7321c393012641095015d58d2912
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 15:32:06 -0600

    Add initial request spec for Items Index
    - Also renames merchant index request spec for order naming

---
+++ spec/requests/api/v1/items_index_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 5)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items[:data].count).to eq(5)
+
+    binding.pry
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq("item")
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(Text)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+
+      expect(item[:attributes]).to have_key(:created_at)
+      expect(item[:attributes][:created_at]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:updated_at)
+      expect(item[:attributes][:updated_at]).to be_an(String)
+    end
+  end
+end
