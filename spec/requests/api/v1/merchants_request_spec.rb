75ce5dadc907e2b59c2c8ccac4fffb669dd1b8c6
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 14:59:32 -0600

    Update merchant request spec for ruby object assertations

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,22 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_an(String)
+
+      expect(merchant).to have_key(:created_at)
+      expect(merchant[:created_at]).to be_an(String)
+
+      expect(merchant).to have_key(:updated_at)
+      expect(merchant[:updated_at]).to be_an(String)
+    end
   end
 end
