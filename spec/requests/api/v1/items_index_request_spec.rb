04d771e9b06313e05ead7427fcad5a0798a411f9
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 15:32:06 -0600

    Update items index request spec for serialization

--- spec/requests/api/v1/items_index_request_spec.rb
+++ spec/requests/api/v1/items_index_request_spec.rb
@@ -9,35 +9,33 @@ describe "Items API" do
     expect(response).to be_successful
 
     items = JSON.parse(response.body, symbolize_names: true)
-
-    expect(items[:data].count).to eq(5)
-
     binding.pry
+    expect(items[:data].count).to eq(5)
 
     items[:data].each do |item|
       expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(String)
+      expect(item[:id]).to be_a(String)
 
       expect(item).to have_key(:type)
       expect(item[:type]).to eq("item")
 
       expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_an(String)
+      expect(item[:attributes][:name]).to be_a(String)
 
       expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_an(Text)
+      expect(item[:attributes][:description]).to be_a(String)
 
       expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_an(Float)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
 
       expect(item[:attributes]).to have_key(:merchant_id)
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
 
       expect(item[:attributes]).to have_key(:created_at)
-      expect(item[:attributes][:created_at]).to be_an(String)
+      expect(item[:attributes][:created_at]).to be_a(String)
 
       expect(item[:attributes]).to have_key(:updated_at)
-      expect(item[:attributes][:updated_at]).to be_an(String)
+      expect(item[:attributes][:updated_at]).to be_a(String)
     end
   end
 end
