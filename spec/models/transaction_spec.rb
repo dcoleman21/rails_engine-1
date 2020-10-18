3d6246ba8fe349fcb881dbbfb79560aade91b678
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 13:59:20 -0600

    Update transaction spec for removal of card expiration date

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
     it { should validate_presence_of :card }
-    it { should validate_presence_of :card_exp }
     it { should validate_presence_of :result }
   end
 
