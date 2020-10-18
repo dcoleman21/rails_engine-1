0a381a8a88dc8b3dbbcc41130efd71d6d1e8df71
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 17:58:33 -0600

    Update merchant controller for create and destroy
    - Create only renders if merchant can save
    - Destroy has head: no_content declaration

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,12 +8,18 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def create
-    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+    new_merchant = Merchant.new(merchant_params)
+    render json: MerchantSerializer.new(new_merchant) if new_merchant.save
+  end
+
+  def destroy
+    Merchant.destroy(params[:id])
+    head :no_content
   end
 
   private
 
   def merchant_params
-    params.require(:merchant).permit(:name, :created_at, :updated_at)
+    params.permit(:name, :created_at, :updated_at)
   end
 end
