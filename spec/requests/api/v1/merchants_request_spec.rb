8d7c0b977150b6d658068b09733ea944a9dc7055
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-18 17:58:33 -0600

    Update merchant request for show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -29,4 +29,29 @@ describe "Merchants API" do
       expect(merchant[:attributes][:updated_at]).to be_a(String)
     end
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_a(String)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to eq("merchant")
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:created_at)
+    expect(merchant[:attributes][:created_at]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:updated_at)
+    expect(merchant[:attributes][:updated_at]).to be_a(String)
+  end
 end
