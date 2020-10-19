c61d1341808f9c7af5c2877d33591211ba4874c7
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 14:53:04 -0600

    Add route for single finder merchant get

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/:merchant_id/items', to: 'items#index'
+        get 'find', to: 'search#show'
       end
       namespace :items do
         get '/:item_id/merchant', to: 'merchant#show'
