acdc157a8a524b190f9005895c4b258eee6abdd4
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 12:01:31 -0600

    Add namespaced route for merchant:items index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/:merchant_id/items', to: 'items#index'
+      end
       resources :merchants
       resources :items
     end
