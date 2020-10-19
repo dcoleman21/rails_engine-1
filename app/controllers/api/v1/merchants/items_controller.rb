e2d66e3e50778183f240a6f2f630f9f70bec7416
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 13:25:52 -0600

    Update merchants:items index
    - calls merchant.items using model relationships

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
-    render json: ItemSerializer.new(Item.where(merchant: params[:merchant_id]))
+    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
   end
 end
