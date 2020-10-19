86d987b21f8f887d6f1d96aeb85e6b91eb78f1a8
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 15:52:37 -0600

    Add single finder method to Merchant model
    - used this stack overflow to help me understand how to use ActiveRecord 
    to do a partial search on an attribute name: 
    https://stackoverflow.com/questions/23115192/search-active-record-for-partial-strings-in-a-column

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def self.single_finder(attribute, query)
+    Merchant.where("LOWER(#{attribute}) LIKE ?", "%#{query.downcase}%").first
+  end
 end
