cb48eb7c2b290eb11701d9549045864fb1fa0eec
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-19 18:00:29 -0600

    Add multi finder method to item model
    - Mimics single finder
    - Needs refactor

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,4 +16,14 @@ class Item < ApplicationRecord
       Item.where("#{attribute} ILIKE ?", "%#{query}%").first
     end
   end
+
+  def self.multi_finder(attribute, query)
+    if attribute == "created_at" || attribute == "updated_at"
+      Item.where("to_char(#{attribute},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{query}%")
+    elsif attribute == "unit_price"
+      Item.where("to_char(#{attribute}, '999999999.99') ILIKE ?", "%#{query}%")
+    else
+      Item.where("#{attribute} ILIKE ?", "%#{query}%")
+    end
+  end
 end
