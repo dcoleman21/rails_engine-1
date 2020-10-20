16839384de072b0a1836810542a5d639f7761ec1
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-10-20 13:37:39 -0600

    Add scopes to Invoice
    - Default orders ascending
    - Successful condition on status:"shipped"

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,6 +7,9 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
 
+  default_scope { order(id: :asc) }
+  scope :successful, -> { where(status: "shipped") }
+
   def self.most_expensive(limit = 5, sorting = "DESC")
     select("invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
       .joins(:invoice_items, :transactions)
